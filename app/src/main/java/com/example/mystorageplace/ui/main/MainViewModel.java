159f3ef0027b8837572e7bd9160e5d5f8ef81c82
Piotr Prus <prus.piotr@gmail.com>
2018-08-14 22:54:14 +0200

    unique storages can be added

--- app/src/main/java/com/example/mystorageplace/ui/main/MainViewModel.java
+++ app/src/main/java/com/example/mystorageplace/ui/main/MainViewModel.java
@@ -2,35 +2,69 @@ package com.example.mystorageplace.ui.main;
 
 import android.arch.lifecycle.LiveData;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.example.mystorageplace.base.BaseViewModel;
 import com.example.mystorageplace.data.model.Storage;
 import com.example.mystorageplace.ui.main.storage.StorageListAdapter;
 import com.example.mystorageplace.utils.SingleLiveEvent;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainViewModel extends BaseViewModel {
 
     private static final String TAG = MainViewModel.class.getSimpleName();
 
-    private DatabaseReference mDatabase;
-    public StorageListAdapter storageListAdapter;
+    private DatabaseReference mDatabaseReference;
+    private StorageListAdapter storageListAdapter;
     private SingleLiveEvent _openStorageCreationDialog = new SingleLiveEvent();
+    private ChildEventListener mChildEventListener;
 
     public LiveData openStorageCreationDialog() {
         return _openStorageCreationDialog;
     }
 
     public MainViewModel() {
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference().child("storage-places");
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//                GenericTypeIndicator<HashMap<Storage>> genericTypeIndicator = new GenericTypeIndicator<List<Storage>>(){};
+//                List<Storage> storageList = dataSnapshot.getValue(genericTypeIndicator);
+//                storageListAdapter.setData(storageList);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
     }
 
     public void setStorageListAdapter(StorageListAdapter storageListAdapter) {
@@ -38,28 +72,7 @@ public class MainViewModel extends BaseViewModel {
     }
 
     public void fetchStorages() {
-        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    storageListAdapter.setData(toStorageList(dataSnapshot));
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.d(TAG, "Fetching storage list failed: "  + databaseError);
-            }
-        });
-    }
-
-    private List<Storage> toStorageList(DataSnapshot dataSnapshot) {
-        List<Storage> storageList = new ArrayList<>();
-        for (DataSnapshot child : dataSnapshot.getChildren()) {
-            Storage storage = child.getValue(Storage.class);
-            storageList.add(storage);
-        }
-        return storageList;
+        mDatabaseReference.addChildEventListener(mChildEventListener);
     }
 
     public void addNewStorage() {
@@ -72,6 +85,6 @@ public class MainViewModel extends BaseViewModel {
 
     public void writeToFirebase(String storageName){
         Storage storage = new Storage(storageName, new ArrayList<>());
-        mDatabase.child("storage-places").setValue(storage);
+        mDatabaseReference.push().setValue(storage);
     }
 }
